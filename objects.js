6c390923a18f36c6afc38aef39e057c72abcb696
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-08 03:03:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {};
 playlist['Tommy Genesis'] = 'Rainbow';
 
 function updatePlaylist(playlist,artistName,songTitle) {
-  playlist.artistName = songTitle;
+  playlist.artistName = 'songTitle';
   return playlist;
 }
 
